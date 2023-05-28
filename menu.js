693750b7451b13dacbfa3ab573241c22e6cb2eee
jeovaro <jovaresro@gmail.com>
2023-05-28 00:37:53 -0600

    Summary

---
+++ menu.js
@@ -0,0 +1,20 @@
+contentMenu.innerHTML += `
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Proyecto Jeremy Ovares Roig</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+        <a class="nav-link" href="estudiante.html">Estudiante</a>
+        <a class="nav-link" href="profesor.html">Profesor</a>
+        <a class="nav-link" href="grupo.html">Grupo</a>
+        <a class="nav-link" href="curso.html">Curso</a>
+      </div>
+    </div>
+  </div>
+</nav>
+            `;      
